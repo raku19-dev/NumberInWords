e5cdd16451e13d667c621253d891017915ba9923
Pawel Rakowiecki <pawel.rakowiecki@ubs.com>
2021-03-02 17:27:51 +0100

    fixing output plus running roWords for cents

--- NumberInWords/NumberInWords/Program.cs
+++ NumberInWords/NumberInWords/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace NumberInWords
 {
@@ -7,11 +8,39 @@ namespace NumberInWords
     {
         static void Main(string[] args)
         {
+            char[] delimiterChars = { ','};//, '.', ':'};
             EnglishWordifiedNumber numInWord = new EnglishWordifiedNumber();
-            TextReader tIn = Console.In;
             Console.WriteLine("Enter number of dollars which you want to convert to have it written in English");
-            string number = tIn.ReadLine();
-            Console.WriteLine("Output " + number + " is "+ numInWord.toWords(Int64.Parse(number)));
+            try
+            {
+
+                string number = Console.In.ReadLine();
+                if (number == "0")
+                {
+                    Console.WriteLine("The number in currency fomat is \nZero Only");
+                }
+                else
+                {
+                    string[] words = number.Split(delimiterChars);
+                    string output = "Output " + number + " is ";
+                    output += numInWord.toWords(Int64.Parse(words[0]));
+                    output +=  words[0] == "1" ? " dollar" : " dollars";
+                    if (words.Length > 1)
+                    {
+                        output += " and ";
+                        if (words[1].Length == 1)
+                            words[1] += "0";
+                        output += numInWord.toWords(Int64.Parse(words[1]));
+                        output += words[1] == "01"  ? " cent" : " cents";
+
+                    }
+                    Console.WriteLine(output);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
